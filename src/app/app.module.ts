7e6ad5707a2102267be928894e1e230ced8d0338
suchy1713 <wawrzynow.piotr@gmail.com>
2019-10-17 21:41:44 +0200

    created an html form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+
+import { PopoverModule } from 'ngx-bootstrap/popover';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent,
+      FormComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      PopoverModule.forRoot()
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
