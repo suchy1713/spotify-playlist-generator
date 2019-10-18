89b5138f15e930145ca7c2f753ccf22c4618c83d
suchy1713 <wawrzynow.piotr@gmail.com>
2019-10-18 14:15:05 +0200

    binded form data to the model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { PopoverModule } from 'ngx-bootstrap/popover';
    imports: [
       BrowserModule,
       AppRoutingModule,
-      PopoverModule.forRoot()
+      PopoverModule.forRoot(),
+      FormsModule
    ],
    providers: [],
    bootstrap: [
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,11 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  model: any = {
+    limit: 20,
+    min_acousticness: 0,
+    min_danceability: 0,
+    min_energy: 0,
+    min_instrumentalness: 0,
+    min_liveness: 0,
+    min_popularity: 0,
+    min_speechiness: 0,
+    min_valence: 0,
+    max_acousticness: 1,
+    max_danceability: 1,
+    max_energy: 1,
+    max_instrumentalness: 1,
+    max_liveness: 1,
+    max_popularity: 100,
+    max_speechiness: 1,
+    max_valence: 1,   
+  };
+  generated: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   
+  }
+
+  sendForm(){
+    console.log(this.model);
+    this.generated = true;
+  }
+
+  sended(){
+    return this.generated;
   }
 
 }
