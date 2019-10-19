36f77f68fc8a7f4110e08f57a9138608f7681b86
suchy1713 <wawrzynow.piotr@gmail.com>
2019-10-19 12:38:37 +0200

    switched to reactive form and added validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,8 @@ import { PopoverModule } from 'ngx-bootstrap/popover';
       BrowserModule,
       AppRoutingModule,
       PopoverModule.forRoot(),
-      FormsModule
+      FormsModule,
+      ReactiveFormsModule
    ],
    providers: [],
    bootstrap: [
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,35 +7,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  model: any = {
-    limit: 20,
-    min_acousticness: 0,
-    min_danceability: 0,
-    min_energy: 0,
-    min_instrumentalness: 0,
-    min_liveness: 0,
-    min_popularity: 0,
-    min_speechiness: 0,
-    min_valence: 0,
-    max_acousticness: 1,
-    max_danceability: 1,
-    max_energy: 1,
-    max_instrumentalness: 1,
-    max_liveness: 1,
-    max_popularity: 100,
-    max_speechiness: 1,
-    max_valence: 1,   
-  };
+  paramsForm: FormGroup;
   generated: boolean = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+   }
 
   ngOnInit() {
-  
+    this.paramsForm = this.fb.group({
+      limit: [20, [Validators.required, Validators.min(1), Validators.max(100)]],
+      min_acousticness: [0, [Validators.required, Validators.min(0), Validators.max(1)]],
+      min_danceability: [0, [Validators.required, Validators.min(0), Validators.max(1)]],
+      min_energy: [0, [Validators.required, Validators.min(0), Validators.max(1)]],
+      min_instrumentalness: [0, [Validators.required, Validators.min(0), Validators.max(1)]],
+      min_liveness: [0, [Validators.required, Validators.min(0), Validators.max(1)]],
+      min_popularity: [0, [Validators.required, Validators.min(0), Validators.max(1)]],
+      min_speechiness: [0, [Validators.required, Validators.min(0), Validators.max(1)]],
+      min_valence: [0, [Validators.required, Validators.min(0), Validators.max(1)]],
+      max_acousticness: [1, [Validators.required, Validators.min(0), Validators.max(1)]],
+      max_danceability: [1, [Validators.required, Validators.min(0), Validators.max(1)]],
+      max_energy: [1, [Validators.required, Validators.min(0), Validators.max(1)]],
+      max_instrumentalness: [1, [Validators.required, Validators.min(0), Validators.max(1)]],
+      max_liveness: [1, [Validators.required, Validators.min(0), Validators.max(1)]],
+      max_popularity: [100, [Validators.required, Validators.min(0), Validators.max(100)]],
+      max_speechiness: [1, [Validators.required, Validators.min(0), Validators.max(1)]],
+      max_valence: [1, [Validators.required, Validators.min(0), Validators.max(1)]]
+    }, { validator: this.minGreaterThanMaxValidator});
+  }
+
+  minGreaterThanMaxValidator(g: FormGroup) {
+    var errors: {[k: string]: any} = {};
+
+    if(g.get('min_acousticness').value > g.get('max_acousticness').value){
+      errors.acousticness = true;
+    }
+
+    if(g.get('min_danceability').value > g.get('max_danceability').value){
+      errors.danceability = true;
+    }
+
+    if(g.get('min_energy').value > g.get('max_energy').value){
+      errors.energy = true;
+    }
+
+    if(g.get('min_instrumentalness').value > g.get('max_instrumentalness').value){
+      errors.instrumentalness = true;
+    }
+
+    if(g.get('min_liveness').value > g.get('max_liveness').value){
+      errors.liveness = true;
+    }
+
+    if(g.get('min_popularity').value > g.get('max_popularity').value){
+      errors.popularity = true;
+    }
+
+    if(g.get('min_speechiness').value > g.get('max_speechiness').value){
+      errors.speechiness = true;
+    }
+
+    if(g.get('min_valence').value > g.get('max_valence').value){
+      errors.valence = true;
+    }
+    return errors;
+  }
+
+  createForm(){
+
   }
 
   sendForm(){
-    console.log(this.model);
+    console.log(this.paramsForm.value);
     this.generated = true;
   }
 
