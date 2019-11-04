a81a4f625286ddd20fefa4095b78b4d8922cd988
suchy1713 <wawrzynow.piotr@gmail.com>
2019-11-04 11:59:20 +0100

    added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { FormComponent } from './form/form.component';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { MainComponent } from './main/main.component';
 import { DatePipe } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavbarComponent,
       FormComponent,
-      MainComponent
+      MainComponent,
+      FooterComponent
    ],
    imports: [
       BrowserModule,
